e95ab395a2f118ef9389ea60fa507ac81777ec93
mattimengo <mattia.mengoni@edu.unifi.it>
2025-07-24 15:27:59 +0200

    fix codice c++ -> OOP

--- avatar.cpp
+++ avatar.cpp
@@ -6,266 +6,191 @@
 
 
 
-void movement::up(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        pos_y -= step;  // Movimento verso l'alto
-        std::cout << "W pressed: New pos_y = " << pos_y << std::endl;
-    }
-
-    //bordo superiore
-    if (pos_y < max_heightchngd) {
-        pos_y = clone_y;
-    }
-}
-
-void movement::down(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        pos_y += step; // Movimento verso il basso
-        std::cout << "S pressed: New pos_y = " << pos_y << std::endl;
-    }
-
-    //bordo inferiore
-    if (pos_y > min_heightchngd) {
-        pos_y = c_y;
-    }
-
-    //recinto alto sx
-    if (pos_y > recintos_y && pos_x < corner1_x) {
-        pos_y = clones_y;
-    }
-
-    //recinto alto dx
-    if (pos_y > recintos_y && pos_x > corner2_x) {
-        pos_y = clones_y;
-    }
-}
-
-void movement::left(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        pos_x -= step; // Movimento verso sinistra
-        std::cout << "A pressed: New pos_x = " << pos_x << std::endl;
-    }
-
-    //bordo sinistro
-    if (pos_x < min_widhtchngd) {
-        pos_x = c_x;
-    }
-
-
-    //recinto verticale sx
-    if (pos_x < corner1_x && pos_y > recintos_y) {
-        pos_x = clones_x;
-    }
-
-
-}
-
-void movement::right(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        pos_x += step; // Movimento verso destra
-        std::cout << "D pressed: New pos_x = " << pos_x << std::endl;
-    }
-
-    //bordo destro
-    if (pos_x > max_widhtchngd) {
-        pos_x = clone_x;
-    }
-
-
-    //recinto verticale dx
-    if (pos_x > corner2_x && pos_y > recintos_y) {
-        pos_x = corner2_x;
-
-    }
-
-
-}
-
-
-
-
-void movint::right_i(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        pos_xr += stepr;  // Movimento verso l'alto
-        std::cout << "D pressed: New pos_x = " << pos_xr << std::endl;
-    }
-
-    //bordo destro
-    if (pos_xr > max_widht) {
-        pos_xr = clone_maxw;
-    }
-
-
-}
-
-void movint::left_i(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        pos_xr -= stepr; // Movimento verso sinistra
-        std::cout << "A pressed: New pos_x = " << pos_xr << std::endl;
-    }
-
-    //bordo sinistro
-    if (pos_xr < corner2r) {
-        pos_xr = clone_crn2;
-    }
-
-}
-
-
+movgame::movgame(): posx_g(928.0f), posy_g(600.0f), offset(1.0f),
+        cx1(872.0f), cx2(960.0f), cx22(970.0f), cx3(1627.0f), cx33(1640.0f),
+        cx4(1760.0f), cx44(1780.0f), cx5(1750.0f), cx6(1452.0f), cx62(1460.0f),
+        cx7(1530.0f), cx8(872.0f), cx9(1135.0f), cx10(430.0f), cx11(550.0f),
+        cy1(620.0f), clonecy1(620.0f), cy2(772.0f), cy3(585.0f), clonecy3(585.0f),
+        cy4(220.0f), cy44(200.0f), cy5(422.0f), clonecy5(422.0f),
+        cy3N(450.0f),
+        bsx(23.0f),
+        zoy(220.0f), clonezoy(220.0f),
+        gy1(750.0f), gy2(680.0f),
+        GY1(750.0f),
+        lx(1120.0f), ly(770.0f),
+        gx(960.0f), gy(400.0f),
+        gatef(false), gatel(false), gateg(false),
+		gatec(false), gatem(false), gateb(false), gatez(false) {}
 
 
 void movgame::up_g(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        posy_g -= offset;  // Movimento verso l'alto
-        std::cout << "W pressed: New pos_y = " << posy_g << std::endl;
-    }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            posy_g -= offset;  // Movimento verso l'alto
+            std::cout << "W pressed: New pos_y = " << posy_g << std::endl;
+        }
 
-    //limite con zoo
-    //if (posy_g < zoy) {
-        //limite
-    //    posy_g = clonezoy;
-        //collegamento con lo zoo
-    //}
+        //limite con zoo
+        //if (posy_g < zoy) {
+            //limite
+        //    posy_g = clonezoy;
+            //collegamento con lo zoo
+        //}
 
-    //limite con cy3 a sx
-    if (posy_g < cy3 && posx_g < cx1) {
-        posy_g = clonecy3;
-    }
-
-
-    //limite con cy3 tra cx2 e cx6
-    if (posx_g > cx2 && posx_g < cx6) {
-        if (posy_g < cy3 && posy_g > cy4) {
+        //limite con cy3 a sx
+        if (posy_g < cy3 && posx_g < cx1) {
             posy_g = clonecy3;
         }
-    }
 
 
-    //limite con cy3 tra cx7 e cx5
-    if (posx_g > cx7 && posx_g < cx5) {
-        if (posy_g < cy3) {
-            posy_g = clonecy3;
+        //limite con cy3 tra cx2 e cx6
+        if (posx_g > cx2 && posx_g < cx6) {
+            if (posy_g < cy3 && posy_g > cy4) {
+                posy_g = clonecy3;
+            }
         }
+
+
+        //limite con cy3 tra cx7 e cx5
+        if (posx_g > cx7 && posx_g < cx5) {
+            if (posy_g < cy3) {
+                posy_g = clonecy3;
+            }
+        }
+
+
+
+
+
     }
 
-
-
-
-
-}
 
 void movgame::down_g(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        posy_g += offset; // Movimento verso il basso
-        std::cout << "S pressed: New pos_y = " << posy_g << std::endl;
-    }
-
-
-    //limite con cy2
-    if (posy_g > cy2) {
-        posy_g = cy2;
-    }
-
-    //limte con cy1 sx
-    if (posy_g > cy1 && posx_g < cx1) {
-        posy_g = clonecy1;
-    }
-
-    //limite con cy3 tra cx2 e cx6
-    if (posx_g > cx2 && posx_g < cx6) {
-        if (posy_g > cy3 && posy_g < cy1) {
-            posy_g = clonecy3;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            posy_g += offset; // Movimento verso il basso
+            std::cout << "S pressed: New pos_y = " << posy_g << std::endl;
         }
-    }
 
-    //limite con cy3 tra cx6 e cx7
-    if (posx_g > cx6 && posx_g < cx7) {
-        if (posy_g > cy3 && posy_g < cy1) {
-            posy_g = clonecy3;
+
+        //limite con cy2
+        if (posy_g > cy2) {
+            posy_g = cy2;
         }
-    }
 
-
-
-    //limite con cy3 tra cx7 e cx3
-    if (posx_g > cx7 && posx_g < cx3) {
-        if (posy_g > cy3 && posy_g < cy1) {
-            posy_g = clonecy3;
+        //limte con cy1 sx
+        if (posy_g > cy1 && posx_g < cx1) {
+            posy_g = clonecy1;
         }
-    }
 
-    //limite con cy4 tra cx9 e cx6
-    if (posx_g > cx9 && posx_g < cx6) {
-        if (posy_g > cy4 && posy_g < cy3) {
-            posy_g = clonecy5;
+        //limite con cy3 tra cx2 e cx6
+        if (posx_g > cx2 && posx_g < cx6) {
+            if (posy_g > cy3 && posy_g < cy1) {
+                posy_g = clonecy3;
+            }
         }
-    }
+
+        //limite con cy3 tra cx6 e cx7
+        if (posx_g > cx6 && posx_g < cx7) {
+            if (posy_g > cy3 && posy_g < cy1) {
+                posy_g = clonecy3;
+            }
+        }
 
 
-    //limite con cy4 tra cx2 e cx6
-    if (posy_g < cy4 && posy_g > zoy && posx_g > cx2 && posx_g < cx6) {
-        posy_g = zoy;
-    }
 
-    //limite con cy1 tra cx3 e cx4
-    if (posy_g > cy1 && posy_g < cy2 && posx_g > cx3 && posx_g < cx4) {
-        posy_g = cy1;
-    }
-}
+        //limite con cy3 tra cx7 e cx3
+        if (posx_g > cx7 && posx_g < cx3) {
+            if (posy_g > cy3 && posy_g < cy1) {
+                posy_g = clonecy3;
+            }
+        }
+
+        //limite con cy4 tra cx9 e cx6
+        if (posx_g > cx9 && posx_g < cx6) {
+            if (posy_g > cy4 && posy_g < cy3) {
+                posy_g = clonecy5;
+            }
+        }
+
+
+        //limite con cy4 tra cx2 e cx6
+        if (posy_g < cy4 && posy_g > zoy && posx_g > cx2 && posx_g < cx6) {
+            posy_g = zoy;
+        }
+
+        //limite con cy1 tra cx3 e cx4
+        if (posy_g > cy1 && posy_g < cy2 && posx_g > cx3 && posx_g < cx4) {
+            posy_g = cy1;
+        }
+    }    
+
+    
 
 void movgame::left_g(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        posx_g -= offset; // Movimento verso sinistra
-        std::cout << "A pressed: New pos_x = " << posx_g << std::endl;
-    }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            posx_g -= offset; // Movimento verso sinistra
+            std::cout << "A pressed: New pos_x = " << posx_g << std::endl;
+        }
 
-    //limite con cx1 basso
-    if (posx_g < cx1 && posy_g > cy1) {
-        posx_g = cx1;
-    }
+        //limite con cx1 basso
+        if (posx_g < cx1 && posy_g > cy1) {
+            posx_g = cx1;
+        }
 
-    //limite con cx1 alto
-    if (posx_g < cx1 && posy_g < cy3) {
-        posx_g = cx1;
-    }
+        //limite con cx1 alto
+        if (posx_g < cx1 && posy_g < cy3) {
+            posx_g = cx1;
+        }
 
-    //limite con cy3 tra cx6 e cx7
-    if (posx_g > cx6 && posx_g < cx7 && posy_g < cy3) {
-        posx_g = cx62;
-    }
-
-
-    //limite con bosco
-    //if (posx_g < bsx) {
-     //   posx_g = bsx;
-    //}
+        //limite con cy3 tra cx6 e cx7
+        if (posx_g > cx6 && posx_g < cx7 && posy_g < cy3) {
+            posx_g = cx62;
+        }
 
 
+        //limite con bosco
+        //if (posx_g < bsx) {
+         //   posx_g = bsx;
+        //}
 
 
-}
 
+
+    }    
+
+    
 void movgame::right_g(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        posx_g += offset; // Movimento verso destra
-        std::cout << "D pressed: New pos_x = " << posx_g << std::endl;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            posx_g += offset; // Movimento verso destra
+            std::cout << "D pressed: New pos_x = " << posx_g << std::endl;
+        }
+
+        //limite con cx2 basso
+        if (posx_g > cx2 && posy_g > cy1) {
+            posx_g = cx2;
+        }
+
+
+        //limite con cx5
+        if (posx_g > cx5) {
+            posx_g = cx5;
+        }
+
+        //limite con cx2 tra cy3 e cy4
+        if (posx_g > cx2 && posx_g < cx6 && posy_g < cy3 && posy_g > cy4) {
+            posx_g = cx2;
+        }
+
+
+
+
+
+
+
+
     }
+   
 
-    //limite con cx2 basso
-    if (posx_g > cx2 && posy_g > cy1) {
-        posx_g = cx2;
-    }
-
-
-    //limite con cx5
-    if (posx_g > cx5) {
-        posx_g = cx5;
-    }
-
-    //limite con cx2 tra cy3 e cy4
-    if (posx_g > cx2 && posx_g < cx6 && posy_g < cy3 && posy_g > cy4) {
-        posx_g = cx2;
-    }
+    
 
 
 
@@ -274,175 +199,12 @@ void movgame::right_g(sf::RenderWindow& window) {
 
 
 
-}
-
-//funzione che gestisce il gate del fienile
-void movgame::gate_fienile(sf::RenderWindow& window) {
-    if (posx_g > cx10 && posx_g < cx11) {
-        gatef = true;
-    }
-    else {
-        gatef = false;
-    }
-}
-
-//funzione che gestisce il gate del laghetto
-void movgame::gate_laghetto(sf::RenderWindow& window) {
-    if (posx_g > cx33 && posx_g < cx44) {
-        gatel = true;
-    }
-    else {
-        gatel = false;
-    }
-}
-
-//funzione che gestisce il gate del girasole
-void movgame::gate_girasoli(sf::RenderWindow& window) {
-    if (posx_g > cx1 && posx_g < cx2 && posy_g < cy3N && posy_g > cy4) {
-        gateg = true;
-    }
-    else {
-        gateg = false;
-    }
-}
-
-//funzione che gestisce il gate del campo
-void movgame::gate_campo(sf::RenderWindow& window) {
-    if (posx_g > cx1 && posx_g < cx2 && posy_g > GY1) {
-        gatec = true;
-    }
-    else {
-        gatec = false;
-    }
-}
-
-//funzione che gestisce il gate del magazzino
-void movgame::gate_magazzino(sf::RenderWindow& window) {
-    if (posx_g > 1345 && posx_g <= 1461 && posy_g <= 222) {
-        gatem = true;
-    }
-    else {
-        gatem = false;
-    }
-}
-
-//funzione che gestisce il gate del bosco
-void movgame::gate_bosco(sf::RenderWindow& window) {
-    //limite con bosco
-    if (posx_g < bsx) {
-        gateb = true;
-    }
-    else {
-        gateb = false;
-    }
-}
-
-//funzione che gestisce il gate dello zoo
-void movgame::gate_zoo(sf::RenderWindow& window) {
-    //limite con zoo
-    if (posy_g < zoy) {
-        gatez = true;
-    }
-    else {
-        gatez = false;
-    }
-}
-
-
-
-void movf::up_f(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        posy_f -= offsetf;  // Movimento verso l'alto
-        std::cout << "W pressed: New pos_y = " << posy_f << std::endl;
-    }
-
-    //limite con fienile
-    if (posy_f < fy) {
-        posy_f = fy;
-    }
-
-
-
-
-
-}
-
-void movf::down_f(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        posy_f += offsetf; // Movimento verso il basso
-        std::cout << "S pressed: New pos_y = " << posy_f << std::endl;
-    }
-
-    if (posy_f > ry) {
-        posy_f = ry;
-    }
-}
-
-
-
-
-void movr::left_r(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        posx_r -= stepr; // Movimento verso sinistra
-        std::cout << "A pressed: New pos_x = " << posx_r << std::endl;
-    }
-    //bordo sinistro
-    if (posx_r < rx1) {
-        posx_r = rx1;
-    }
-}
-
-void movr::right_r(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        posx_r += stepr;  // Movimento verso destra
-        std::cout << "D pressed: New pos_x = " << posx_r << std::endl;
-    }
-
-    //bordo destro
-    if (posx_r > rx2) {
-        posx_r = rx2;
-    }
-}
-
-
-
-
-
-void joincmdmenu::join() {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
-        bool cmdjoined = true;
-    }
-}
 
 
 
 
 
 
-
-void movc::left_c(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::C)) {
-        posx_c -= stepc; // Movimento verso sinistra
-        std::cout << "C pressed: New pos_x = " << posx_c << std::endl;
-    }
-
-    //bordo sinistro
-    if (posx_c < bsx) {
-        posx_c = bsx;
-    }
-}
-
-void movc::right_c(sf::RenderWindow& window) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::V)) {
-        posx_c += stepc; // Movimento verso sinistra
-        std::cout << "V pressed: New pos_x = " << posx_c << std::endl;
-    }
-
-    //bordo destro
-    if (posx_c > bdx) {
-        posx_c = bdx;
-    }
-}
 
 
 
--- avatar.h
+++ avatar.h
@@ -5,168 +5,12 @@
 #ifndef AVATAR_H
 #define AVATAR_H
 
-//classe per il movimento sulla mappa
-class movement {
-
-public:
-	//costruttore
-	//movimento sulla mappa
-	movement() {
-		pos_x = 907.0f;
-		pos_y = 439.0f;
-		step = 1.0f;
-		clone_y = 330.0f;
-		c_y = 764.0f;
-		clone_x = 1768.0f;
-		c_x = -60.0f;
-		max_heightchngd = 330.0f;
-		min_heightchngd = 764.0f;
-		max_widhtchngd = 1768.0f;
-		min_widhtchngd = -60.0f;
-		recintos_y = 370.0f;
-		corner1_x = 770.0f;
-		corner2_x = 990.0f;
-		clones_y = 370.0f;
-		clones_x = 770.0f;
-	}
-
-	//metodi
-	void up(sf::RenderWindow& window);
-	void down(sf::RenderWindow& window);
-	void left(sf::RenderWindow& window);
-	void right(sf::RenderWindow& window);
-
-
-
-	//membri
-	float pos_x;
-	float pos_y;
-	float step;
-	float clone_y;
-	float c_y;
-	float clone_x;
-	float c_x;
-	float max_heightchngd;
-	float min_heightchngd;
-	float max_widhtchngd;
-	float min_widhtchngd;
-	float recintos_y;
-	float corner1_x;
-	float corner2_x;
-	float clones_x;
-	float cr_x;
-	float clones_y;
-	float cr_y;
-
-	//distruttore
-	~movement() {};
-
-
-};
-
-//classe per il movimento interno al recinto dx
-class movint {
-
-public:
-
-	//movimento interno recinto dx
-	movint() {
-		pos_xr = 500.0f;
-		pos_yr = 650.0f;
-		stepr = 1.0f;
-		max_widht = 1768.0f;
-		corner2r = 990.0f;
-		clone_maxw = 1768.0f;
-		clone_crn2 = 990.0f;
-	}
-
-	//metodi
-	//void up_i(sf::RenderWindow& window);
-	//void down_i(sf::RenderWindow& window);
-	void left_i(sf::RenderWindow& window);
-	void right_i(sf::RenderWindow& window);
-
-
-	//membri
-	float pos_xr;
-	float pos_yr;
-	float stepr;
-	float max_widht;
-	float clone_maxw;
-	float corner2r;
-	float clone_crn2;
-
-	//distruttore
-	~movint() {};
-
-
-};
 
 //classe per il movimento di map2
 class movgame {
 public:
 	//costruttore
-	movgame() {
-		posx_g = 928.0f;
-		posy_g = 600.0f;
-		offset = 1.0f;
-
-		cx1 = 872.0f;
-		cx2 = 960.0f;
-		cx22 = 970.0f;
-		cx3 = 1627.0f;
-		cx33 = 1640.0f;
-		cx4 = 1760.0f;
-		cx44 = 1780.0f;
-		cx5 = 1750.0f;
-		cx6 = 1452.0f;
-		cx62 = 1460.0f;
-		cx7 = 1530.0f;
-		cx8 = 872.0f;
-		cx9 = 1135.0f;
-		cx10 = 430.0f;
-		cx11 = 550.0f;
-
-		cy1 = 620.0f;
-		clonecy1 = 620.0f;
-		cy2 = 772.0f;
-		cy3 = 585.0f;
-		clonecy3 = 585.0f;
-		cy4 = 220.0f;
-		cy44 = 200.0f;
-		cy5 = 422.0f;
-		clonecy5 = 422.0f;
-
-		cy3N = 450.0f;
-
-
-
-		bsx = 23.0f; //bosco
-
-		zoy = 220.0f;  //zoo
-		clonezoy = 220.0f;
-
-		//posizioni di default nel gate recinto
-		gy1 = 750.0f;
-		gy2 = 680.0f;
-
-		gatef = false;
-
-
-
-		//posizioni di default nel gate laghetto
-		lx = 1120.0f;
-		ly = 770.0f;
-
-
-		//posizioni di default nel campo di girasoli
-		gx = 960.0f;
-		gy = 400.0f;
-
-
-		GY1 = 660.0f;
-	}
-
+	movgame();
 
 	//metodi
 	void up_g(sf::RenderWindow& window);
@@ -174,15 +18,11 @@ public:
 	void left_g(sf::RenderWindow& window);
 	void right_g(sf::RenderWindow& window);
 
-	void gate_fienile(sf::RenderWindow& window);
-	void gate_laghetto(sf::RenderWindow& window);
-	void gate_girasoli(sf::RenderWindow& window);
-	void gate_campo(sf::RenderWindow& window);
-	void gate_magazzino(sf::RenderWindow& window);
-	void gate_bosco(sf::RenderWindow& window);
-	void gate_zoo(sf::RenderWindow& window);
+	// Getter per posizione
+    float getPosX() const { return posx_g; }
+    float getPosY() const { return posy_g; }
 
-
+private:
 	//membri
 	float posx_g;
 	float posy_g;
@@ -245,109 +85,4 @@ public:
 
 
 
-//classe per il movimento nel recinto del fienile
-class movf {
-public:
-	movf() {
-		posx_f = 455.0f;
-		posy_f = 625.0f;
-		offsetf = 1.0f;
-
-
-		fy = 390.0f;
-		ry = 435.0f;
-		cxf = 430.0f;
-	}
-
-	//metodi
-	void up_f(sf::RenderWindow& window);
-	void down_f(sf::RenderWindow& window);
-
-
-
-	//membri
-	float posx_f;
-	float posy_f;
-	float offsetf;
-
-	float fy;
-	float ry;
-	float cxf;
-
-};
-
-//classe per il movimento interno al recinto del fienile dx in basso
-class movr {
-public:
-	movr() {
-		rx1 = 960.0f;
-		rx2 = 1630.0f;
-
-		stepr = 1.0f;
-		posx_r = 1515.0f;
-		posy_r = 800.0f;
-	}
-
-	//metodi
-	void left_r(sf::RenderWindow& window);
-	void right_r(sf::RenderWindow& window);
-
-
-
-	//membri
-	float rx1;
-	float rx2;
-
-
-	float stepr;
-	float posx_r;
-	float posy_r;
-
-
-};
-
-//classe per il movimento interno al campo sx
-class movc {
-public:
-	movc() {
-		posx_c = 750.0f;
-		posy_c = 800.0f;
-		stepc = 1.0f;
-
-		bsx = 23.0f;
-		bdx = 776.0f;
-	}
-	//metodi
-	void left_c(sf::RenderWindow& window);
-	void right_c(sf::RenderWindow& window);
-
-
-
-	//membri
-	float posx_c;
-	float posy_c;
-	float stepc;
-	float bsx;
-	float bdx;
-
-};
-
-
-
-
-
-
-//classe per l'interazione con il menu dei comandi
-class joincmdmenu {
-public:
-
-	joincmdmenu() {
-		cmdjoined = false;
-	}
-
-	void join();
-
-	bool cmdjoined;
-
-};
 #endif // !X
